c15cda08fc1102a6c745eadcbac0559f510cc839
imtiaz <imtiazuddinedu@gmail.com>
2023-10-04 00:31:45 +0600

    update

--- deposit.js
+++ deposit.js
@@ -1,30 +1,54 @@
 let depositInput = document.getElementById('deposit-input');
 let withdrawInput = document.getElementById('withdraw-input');
-const depositBtn = document.getElementById('deposit-btn')
-const withdrawBtn = document.getElementById('withdraw-btn')
-
+const depositBtn = document.getElementById('deposit-btn');
+const withdrawBtn = document.getElementById('withdraw-btn');
+let allDeposit = [];
+let allWithdraw=[];
+let WithdrawHistory;
+let depositHistory;
 let newBalance;
 let newWithdrawAmount;
+let date = new Date();
+
 
 depositBtn.addEventListener('click', function(){
   let currentDeposit = parseFloat(document.getElementById('deposit-total').innerText)
-  depositAmount = parseFloat(depositInput.value)
+
+  depositAmount = parseFloat(depositInput.value);
   newBalance = currentDeposit + depositAmount; 
-  document.getElementById('deposit-total').innerText = newBalance
-  document.getElementById('balance-total').innerText = newBalance
+  document.getElementById('deposit-total').innerText = newBalance;
+  document.getElementById('balance-total').innerText = newBalance;
+
+  allDeposit.push(depositAmount)
+
+  const p = document.createElement('p')
+  p.classList.add('text-sm')
+  allDeposit.forEach(function(singleDeposit, index){
+    p.innerText = `${index + 1}---  ${singleDeposit} Tk ${date.toDateString()} time:  ${date.toLocaleTimeString()}`
+    document.getElementById('dipositHistory').appendChild(p)
+  })
+  
 })
 
+
 withdrawBtn.addEventListener('click', function(){
-  let withdrawAmout = parseFloat(withdrawInput.value)
+  let withdrawAmout = parseFloat(withdrawInput.value);
   if(newBalance < withdrawAmout){
-    alert('Your balance is too low')
+    alert(`Your balance is too low.
+You cannot withdray more amount than your Main balance`)
   }else{
     newBalance -= withdrawAmout
     document.getElementById('balance-total').innerText = newBalance
     let previousWithdrawAmount =  parseFloat(document.getElementById('withdraw-total').innerText);
     let currentWithdrawAmount = previousWithdrawAmount + withdrawAmout
     document.getElementById('withdraw-total').innerText = currentWithdrawAmount
+    allWithdraw.push(withdrawAmout)
+    const p = document.createElement('p')
+    allWithdraw.forEach(function(singleWithdraw, index){
+      p.innerText = `${index + 1}---  ${singleWithdraw} Tk ${date.toDateString()} time:  ${date.toLocaleTimeString()}`
+      document.getElementById('withdrawHistory').appendChild(p)
+    })
+  document.getElementById('withdrawHistory').innerHTML=`Withdraw ammount: ${withdrawAmout} Tk ${d}`;
     
   }
-
 })
